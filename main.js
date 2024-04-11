1a9d8160868b6baa57144f62288dbb5bd07f486b
ImaedaYuto <nowstick0327yuto@gmail.com>
2024-04-11 19:07:30 +0900

    あ

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ for(let area_i = 0; area_i < anime_areas.length ; area_i++){
     const anime_imgs = anime_areas[area_i].querySelectorAll( ".anime");
 
     anime_imgs.forEach((anime_img)=>{
-        anime_img.style.top = Math.random() * 100 + "%" ;
-        anime_img.style.left = Math.random() * 100 + "%";
+        anime_img.style.top = Math.random() * anime_areas[area_i].clientHeight + "px";
+        anime_img.style.left = Math.random() * anime_areas[area_i].clientWidth + "px";
     });
 
 
@@ -16,6 +16,9 @@ for(let area_i = 0; area_i < anime_areas.length ; area_i++){
         let anime_x = anime_imgs[i].getBoundingClientRect().left;
         let anime_y = anime_imgs[i].getBoundingClientRect().top;
         let bounce_rad = Math.random() * Math.PI * 2;
+
+        //ここでアニメーションのスピードを調節できます
+        let anime_speed = 1;
         
         const bounce_anime = ()=>{
         
@@ -30,8 +33,8 @@ for(let area_i = 0; area_i < anime_areas.length ; area_i++){
                 anime_imgs[i].style.top = 2 * (anime_areas[area_i].clientHeight - anime_imgs[i].clientHeight) - anime_y + "px";
             }
 
-            anime_x = anime_x + Math.cos(- bounce_rad);
-            anime_y = anime_y + Math.sin(- bounce_rad);
+            anime_x = anime_x + anime_speed * Math.cos(- bounce_rad);
+            anime_y = anime_y + anime_speed * Math.sin(- bounce_rad);
         
             if(anime_x > 2 * (anime_areas[area_i].clientWidth - anime_imgs[i].clientWidth )){
                 anime_x = 0;
