7aef9bbab388bfbc29bd1f5ef3c9e38b88770674
ImaedaYuto <nowstick0327yuto@gmail.com>
2024-04-13 07:43:35 +0900

    完璧な角度と位置

--- main.js
+++ main.js
@@ -6,19 +6,20 @@ const anime_areas = document.querySelectorAll(".anime-area")
 for(let area_i = 0; area_i < anime_areas.length ; area_i++){
     const anime_imgs = anime_areas[area_i].querySelectorAll( ".anime");
 
-    anime_imgs.forEach((anime_img)=>{
-        anime_img.style.top = Math.random() * anime_areas[area_i].clientHeight + "px";
-        anime_img.style.left = Math.random() * anime_areas[area_i].clientWidth + "px";
-    });
 
 
     for(let i = 0; i < anime_imgs.length; i ++){
+
+        anime_imgs[i].style.top = (Math.random() + i) / anime_imgs.length * anime_areas[area_i].clientHeight + "px";
+        anime_imgs[i].style.left = (Math.random() + i) / anime_imgs.length * anime_areas[area_i].clientWidth + "px";
+
+
         let anime_x = anime_imgs[i].getBoundingClientRect().left;
         let anime_y = anime_imgs[i].getBoundingClientRect().top;
-        let bounce_rad = Math.random() * Math.PI * 2;
+        let bounce_rad = Math.PI * (i / anime_imgs.length - 1 / (anime_imgs.length + 1));
 
         //ここでアニメーションのスピードを調節できます
-        let anime_speed = 1;
+        let anime_speed = .5;
         
         const bounce_anime = ()=>{
         
